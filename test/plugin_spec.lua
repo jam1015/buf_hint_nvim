dd862f3f913e8ef0944a159d2e1ef7de84440329
Jordan Mandel <jordan.mandel@live.com>
2024-05-28 10:57:42 -0400

    made change

--- test/plugin_spec.lua
+++ test/plugin_spec.lua
@@ -1,8 +1,8 @@
-local example = require('nvim-plugin-template').example
-
-describe('neovim plugin', function()
-  it('work as expect', function()
-    local result = example()
-    assert.is_true(result)
-  end)
-end)
+--local example = require('nvim-plugin-template').example
+--
+--describe('neovim plugin', function()
+--  it('work as expect', function()
+--    local result = example()
+--    assert.is_true(result)
+--  end)
+--end)
